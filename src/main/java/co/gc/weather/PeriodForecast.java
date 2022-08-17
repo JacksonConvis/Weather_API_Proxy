f87269202e8eca15cb28c3cb326e75a70adbe72f
Jack Convis <jackconvis@gmail.com>
2022-08-17 19:32:29 -0400

    model

---
+++ src/main/java/co/gc/weather/PeriodForecast.java
@@ -0,0 +1,13 @@
+package co.gc.weather;
+
+public class PeriodForecast {
+    private int temperatureCelcius;
+
+	public int getTemperatureCelcius() {
+		return temperatureCelcius;
+	}
+
+	public void setTemperatureCelcius(int temperatureCelcius) {
+		this.temperatureCelcius = temperatureCelcius;
+	}
+}
---
+++ src/main/java/co/gc/weather/ProxyPeriod.java
@@ -0,0 +1,5 @@
+package co.gc.weather;
+
+public class ProxyPeriod extends PeriodForecast{
+    
+}
